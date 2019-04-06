54dcc91eec9b0b0e09a56a330c7b50e12acb2673
spainhour <msspainhour@gmail.com>
2019-04-05 23:43:01 -0500

    Formats tweets correctly

--- WeatherBot.py
+++ WeatherBot.py
@@ -3,14 +3,19 @@ import datetime
 import tweepy
 from Keys import *
 
+daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
 def main():
     url = "https://api.darksky.net/forecast/1b344af14054572a62de555a58e4bf7d/35.090328, -92.441559"
     data = requests.get(url).json()
-    date = getDate()
-    low_temp = getLowTemp()
-    high_temp = getHighTemp()
-    message = writeMessage(date, low_temp, high_temp)
-#    tweet(message)
+    date = getDate(data)
+    low_temp = getLowTemp(data)
+    high_temp = getHighTemp(data)
+    precip_probability = getPrecipProb(data)
+    weekday = getWeekday(data)
+    summary = getSummary(data)
+    message = writeMessage(date, low_temp, high_temp, precip_probability, weekday, summary)
+    tweet(message)
     print(message)
 
 def tweet(message):
@@ -19,18 +24,34 @@ def tweet(message):
     bot = tweepy.API(account)
     bot.update_status(message)
 
-def getLowTemp():
+def getSummary(data):
+    return(data['daily']['data'][0]['summary'])
+
+def getPrecipProb(data):
+    return(int(data['daily']['data'][0]['precipProbability'])*100)
+
+def getLowTemp(data):
     return(int(data['daily']['data'][0]['temperatureMin']))
 
-def getHighTemp():
+def getHighTemp(data):
     return(int(data['daily']['data'][0]['temperatureMax']))
 
-def getDate():
-    date = data['daily']['data'][0]['time']
-    date = str(datetime.datetime.fromtimestamp(date))
+def getWeekday(data):
+    date = data['currently']['time']
+    weekday = datetime.date.fromtimestamp(date).weekday()
+    weekday = daysOfWeek[weekday]
+    return weekday
+
+def getDate(data):
+    date = data['currently']['time']
+    date = str(datetime.date.fromtimestamp(date))
     return date
 
-def writeMessage(date, high_temp, low_temp):
-    message = "Today is " + str(date) + ". The High for today is " + str(high_temp) + ", and the Low is " + str(low_temp) + "."
+def writeMessage(date, low_temp, high_temp, precip_probability, weekday, summary):
+    return("Today is " + weekday + ", " + str(date) \
+    + ". The high for today is " + str(high_temp) \
+    + ", and the low is " + str(low_temp) \
+    + ". There is a " + str(precip_probability) + "% chance for precipitation today. "
+    + summary)
 
 main()
