e1388a1c6f0eaf1e759762442acc5c350cb77f7d
spainhour <msspainhour@gmail.com>
2019-04-08 13:48:30 -0500

    Weekly forecast on Monday

--- WeatherBot.py
+++ WeatherBot.py
@@ -6,18 +6,22 @@ from Keys import *
 daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 def main():
-    inputQuestion = input("Tweet the daily weather forecast? y/n ")
+    inputQuestion = input("Tweet the weather forecast? y/n ")
     if inputQuestion == "y":
         url = "https://api.darksky.net/forecast/1b344af14054572a62de555a58e4bf7d/35.090328, -92.441559"
         data = requests.get(url).json()
-        date = getDate(data)
-        low_temp = getLowTemp(data)
-        high_temp = getHighTemp(data)
-        precip_probability = getPrecipProb(data)
         weekday = getWeekday(data)
-        summary = getSummary(data)
-        message = writeMessage(date, low_temp, high_temp, precip_probability, weekday, summary)
-#        tweet(message)
+        if weekday == "Monday":
+            date = getDate(data)
+            weeklySummary = getWeeklySummary(data)
+            message = writeMessage(date, weekday, weeklySummary, None, None, None)
+        else:
+            date = getDate(data)
+            low_temp = getLowTemp(data)
+            high_temp = getHighTemp(data)
+            dailySummary = getSummary(data)
+            message = writeMessage(date, weekday, dailySummary, low_temp, high_temp)
+#       tweet(message)
         print(message)
 
 def tweet(message):
@@ -25,6 +29,9 @@ def tweet(message):
     account.set_access_token(access_token, access_secret)
     bot = tweepy.API(account)
     bot.update_status(message)
+
+def getWeeklySummary(data):
+    return(data['daily']['summary'])
 
 def getSummary(data):
     return(data['daily']['data'][0]['summary'])
@@ -49,11 +56,14 @@ def getDate(data):
     date = str(datetime.date.fromtimestamp(date))
     return date
 
-def writeMessage(date, low_temp, high_temp, precip_probability, weekday, summary):
-    return("Today is " + weekday + ", " + str(date) \
-    + ". The high for today is " + str(high_temp) \
-    + ", and the low is " + str(low_temp) \
-    + ". There is a " + str(precip_probability) + "% chance for precipitation today. "
-    + summary)
+def writeMessage(date, weekday, summary, low_temp=None, high_temp=None, precip_probability=None):
+    if weekday == "Monday":
+        return("Today is " + weekday + ", " + str(date) \
+        + ". " + summary)
+    else:
+        return("Today is " + weekday + ", " + str(date) \
+        + ". The high for today is " + str(high_temp) \
+        + ", and the low is " + str(low_temp) + "." \
+        + summary)
 
 main()
